137d2796255256f5becb1c89d14805f72a1cca3f
Alonte96 <lee227264770@gmail.com>
2025-09-12 02:53:48 +0100

    feat: add customizable tokenization rules

--- src/unsegbench/errors.py
+++ src/unsegbench/errors.py
@@ -50,7 +50,7 @@ class OffsetMisalignment(UnsegbenchError):
     take down a whole sweep.
     """
 
-# improved
+# # improved
 
 class TokenizerUnavailable(UnsegbenchError):
     """A tokenizer could not be loaded (gated repo, missing sentencepiece, ...)."""
